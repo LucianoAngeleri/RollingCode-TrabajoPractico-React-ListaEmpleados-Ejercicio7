578d33cb613ad35857afa25ef94cfbd5e28d51e3
Luciano Angeleri <lcnang@gmail.com>
2023-05-31 00:05:56 -0300

    Se añadió el footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import EmpleadoLista from './components/EmpleadoLista';
 function App() {
   return (
     <>
-    <Container>
+    <Container className="py-4">
       <h1 className="display-1">Ejercicio 7</h1>
       <h2 className="display-2">Lista de Empleados</h2>
       <hr />
     </Container>
-    <Container>
+    <Container className="py-4">
       <EmpleadoLista/>
     </Container>
+    <footer className='text-bg-dark text-center py-5'>&copy; Todos los Derechos Reservados 2023</footer>
     </>
   )
 }
